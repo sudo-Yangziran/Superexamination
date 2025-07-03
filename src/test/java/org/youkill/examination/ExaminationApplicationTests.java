ece468dc29a579f98e31ba576e191f365efb10a1
youkill <Yangyoukill@outlook.com>
2025-07-03 17:56:53 +0800

    初始化

---
+++ src/test/java/org/youkill/examination/ExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youkill.examination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
